d1dcc858f569a9dfdfc22b46d404a34d6a262719
yushiwo <zhengrui@otcbtc.com>
2018-10-19 17:32:04 +0800

    ...

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         #
+         # devise :database_authenticatable, :registerable,
+         #        :recoverable, :rememberable, :validatable
 
   has_many :groups
   has_many :posts
